67ee42ff7797acad644889f48f8fd952fc218702
Christopher Langton <chris@langton.cloud>
2026-03-23 18:27:26 +1100

    chore: update flake.nix to v1.12.0

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        version = "1.11.1";
+        version = "1.12.0";
       in
       {
         packages = {
