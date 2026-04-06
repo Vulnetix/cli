de2e6d9eb35494b2b956f2b2c66c32432896f68d
Christopher Langton <chris@vulnetix.com>
2026-04-06 17:19:22 +0000

    chore: update flake.nix to v1.28.6 [skip ci]

--- flake.nix
+++ flake.nix
@@ -11,9 +11,9 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 <<<<<<< HEAD
-        version = "1.28.5";
+        version = "1.28.6";
 =======
-        version = "1.28.5";
+        version = "1.28.6";
 >>>>>>> 5b061d80679c748e0fee1ba1cc3a93962bea1c86
       in
       {
