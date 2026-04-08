4fe56c09a0be98ad84f4e176075a38cecb0fa947
Christopher Langton <chris@vulnetix.com>
2026-04-09 00:21:03 +1000

    chore: update flake.nix to v1.29.0, fix release workflow detached HEAD

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        version = "1.28.9";
+        version = "1.29.0";
       in
       {
         packages = {
