710853be3a4da9cf19e721c22a6f6c7f7c3117d7
Christopher Langton <chris@vulnetix.com>
2026-06-13 15:06:02 +0000

    chore: update flake.nix to v3.28.0 [skip ci]

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        version = "3.27.2";
+        version = "3.28.0";
       in
       {
         packages = {
